4662be2c9122660217b9ecccda016bfba037f7c7
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-05-26 19:29:02 +0600

    implemented profile services

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -1,41 +1,22 @@
-import prisma from "../../db/prisma";
+import { AuthModel } from "../auth/auth.model";
 
 const getProfile = async (userId: string) => {
-  const user = await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
-    select: {
-      name: true,
-      email: true,
-      profile: true,
-    },
-  });
-
+  const user = await AuthModel.findById(userId).select("-password");
   return user;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const updateProfile = async (userId: string, data: any) => {
-  const user = await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
-    select: {
-      profile: true,
-    },
-  });
+  const user = await AuthModel.findById(userId);
 
   if (!user) {
     return null;
   }
-  const result = await prisma.userProfile.update({
-    where: {
-      id: user.profile!.id,
-    },
-    data,
-  });
-  return result;
+
+  const updatedUser = await AuthModel.findByIdAndUpdate(userId, data, {
+    new: true,
+  }).select("-password");
+  return updatedUser;
 };
 
 export const ProfileServices = {
