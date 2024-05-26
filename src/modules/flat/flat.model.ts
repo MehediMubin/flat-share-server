873df06896d082ae910a967c523aac7873508e7f
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-05-26 18:50:38 +0600

    implemented flat model

---
+++ src/modules/flat/flat.model.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+import { TFlat } from "./flat.interface";
+
+const flatSchema = new Schema<TFlat>({
+  location: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  rent: {
+    type: Number,
+    required: true,
+  },
+  numberOfBedrooms: {
+    type: Number,
+    required: true,
+  },
+  amenities: {
+    type: String,
+  },
+  photoUrl: {
+    type: [String],
+  },
+});
+
+export const FlatModel = model<TFlat>("Flat", flatSchema);
