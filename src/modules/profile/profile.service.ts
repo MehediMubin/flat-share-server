65a5f28ebe58222173348f1c15234d35a29b7e4c
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-05-27 12:31:14 +0600

    updateprofile modified

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -1,3 +1,5 @@
+import bcrypt from "bcrypt";
+import config from "../../config";
 import { AuthModel } from "../auth/auth.model";
 
 const getProfile = async (userId: string) => {
@@ -5,18 +7,54 @@ const getProfile = async (userId: string) => {
   return user;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const updateProfile = async (userId: string, data: any) => {
-  const user = await AuthModel.findById(userId);
+  try {
+    const { username, email, oldPassword, newPassword } = data;
 
-  if (!user) {
-    return null;
+    const user = await AuthModel.findById(userId);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    if (oldPassword) {
+      const isMatch = await bcrypt.compare(oldPassword, user.password);
+      if (!isMatch) {
+        throw new Error("Old password is incorrect");
+      }
+      data.password = await bcrypt.hash(
+        newPassword,
+        config.salt_rounds as string,
+      );
+    }
+
+    const updateData: any = {};
+    if (username) updateData.username = username;
+    if (email) updateData.email = email;
+    if (data.password) updateData.password = data.password;
+
+    const updatedUser = await AuthModel.findOneAndUpdate(
+      {
+        _id: userId,
+        $or: [{ username: { $ne: username } }, { email: { $ne: email } }],
+      },
+      updateData,
+      { new: true, runValidators: true, context: "query" },
+    ).select("-password");
+
+    if (!updatedUser) {
+      throw new Error("Username or Email already exists");
+    }
+
+    return updatedUser;
+  } catch (error: any) {
+    if (error.code === 11000) {
+      const key = Object.keys(error.keyValue)[0];
+      const value = error.keyValue[key];
+      throw new Error(`The ${key} "${value}" already exists.`);
+    }
+    throw new Error(error.message);
   }
-
-  const updatedUser = await AuthModel.findByIdAndUpdate(userId, data, {
-    new: true,
-  }).select("-password");
-  return updatedUser;
 };
 
 export const ProfileServices = {
